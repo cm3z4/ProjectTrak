e007413fdd342d88c978c4b3d3961c004167e990
Matthew Poche <Matthewspoche@comcast.net>
2019-01-27 14:15:03 -0600

    changes

--- models/project.js
+++ models/project.js
@@ -2,10 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const projectSchema = new Schema({
-  salesman: { type: String, required: true }
-  // author: { type: String, required: true },
-  // synopsis: String,
-  // date: { type: Date, default: Date.now }
+  salesman: { type: String, required: true },
+  project_name: { type: String, required: true},
+  status: { type: String, required: true },
+  company_name: { type: String, required: true },
+  company_address: { type: String, required: true },
+  contact_name: { type: String, required: true },
+  contact_number: { type: String, required: true },
+  contact_email: { type: String, required: true },
+  estimated_start: { type: Date, required: true },
+  estimated_finish: { type: Date, required: true },
+  estimated_value: { type: String, required: true },
+  discription: { type: String, required: true }
 });
 
 const Project = mongoose.model("Project", projectSchema);
